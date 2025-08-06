a9a00146dd190ac2fe95493d5a4c86dae9941ef0
Karyamsetty-Prajwala <155668778+Karyamsetty-Prajwala@users.noreply.github.com>
2025-08-06 11:45:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
+from langchain_community.tools.tavily_research import TavilySearchResults
+
 
 # NEW: Import for OpenAI Embeddings
 from langchain_openai import OpenAIEmbeddings
